d8f4bcf6b345e6771c18f3964f2c784ab201f247
mettinger <jettinger35@gmail.com>
2026-02-20 08:51:45 -0800

    comment

--- relativity.lean
+++ relativity.lean
@@ -93,13 +93,13 @@ def velocitySq (m k : B) (v : ℝ) : Prop := ∀ (x y : R4), W m k x ∧ W m k y
   spaceDistanceSq x y = v * v * timeDistanceSq x y
 
 -- "m sees eventBody at time t"
-def time (m eventBody : B) (t : ℝ) : Prop := ∃ (x : R4), W m eventBody x ∧ x 3 = t
+def timeOfEvent (m eventBody : B) (t : ℝ) : Prop := ∃ (x : R4), W m eventBody x ∧ x 3 = t
 
 -- Theorem: "In special relativity, different inertial observers may disagree on the temporal order of events"
 theorem temporalOrderParadox : SpecRel B IB Ph W → ∀ (m k : B) (v : ℝ),
   IOb B IB W m ∧ IOb B IB W k ∧ velocitySq B W m k v ∧ v > 0 →
-  ∃ (b1 b2: B) (t1 t2 t3 t4: ℝ), time B W m b1 t1 ∧ time B W m b2 t2 ∧
-  time B W k b1 t3 ∧ time B W k b2 t4 ∧ t1 < t2 ∧ t4 < t3 := by
+  ∃ (b1 b2: B) (t1 t2 t3 t4: ℝ), timeOfEvent B W m b1 t1 ∧ timeOfEvent B W m b2 t2 ∧
+  timeOfEvent B W k b1 t3 ∧ timeOfEvent B W k b2 t4 ∧ t1 < t2 ∧ t4 < t3 := by
     intros h_specRel m k v h_conditions
     have h_contradiction : spaceDistanceSq (0 : R4)
       (EuclideanSpace.single 0 1) < timeDistanceSq (0 : R4) (EuclideanSpace.single 0 1) := by
@@ -116,6 +116,28 @@ theorem temporalOrderParadox : SpecRel B IB Ph W → ∀ (m k : B) (v : ℝ),
     unfold spaceNormSq at h_contradiction; norm_num [ spatial ] at h_contradiction ;
     simp at h_contradiction;
     linarith
+
+
+----------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
