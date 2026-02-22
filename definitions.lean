34cfa114d14e04d2b314b98df78e466055df0843
mettinger <jettinger35@gmail.com>
2026-02-22 06:57:09 -0800

    Co-authored-by: Aristotle (Harmonic) <aristotle-harmonic@harmonic.fun>

--- definitions.lean
+++ definitions.lean
@@ -29,6 +29,31 @@ def timeDistanceSq (p q : R4) : ℝ := (p 3 - q 3) ^ 2
 
 def lightLike (p q : R4) := spaceDistanceSq p q = timeDistanceSq p q
 
+-- "m sees k moving at velocity v"
+def velocitySq (m k : B) (v : ℝ) : Prop := ∀ (x y : R4), W m k x ∧ W m k y →
+  spaceDistanceSq x y = v * v * timeDistanceSq x y
+
+-- "m sees eventBody at time t"
+def timeOfEvent (m eventBody : B) (t : ℝ) : Prop := ∃ (x : R4), W m eventBody x ∧ x 3 = t
+
+-- "m sees eventBody at location x"
+def locOfEvent (m : B) (eventBody : B) (x : R4) : Prop := W m eventBody x
+
+-- "m sees the time between events as t"
+def timeBetweenEvents (m eventBody1 eventBody2 : B) (t : ℝ) : Prop :=
+  ∃ (x1 x2 : R4), W m eventBody1 x1 ∧ W m eventBody2 x2 ∧ timeDistanceSq x1 x2 = t * t
+
+-- "m sees the spatial distance between events as s"
+def spaceBetweenEvents (m eventBody1 eventBody2 : B) (s : ℝ) : Prop :=
+  ∃ (x1 x2 : R4), W m eventBody1 x1 ∧ W m eventBody2 x2 ∧ spaceDistanceSq x1 x2 = s * s
+
+-- the Minkowski metric between two points in R4
+def minkowskiMetricSqPoints (p q : R4) : ℝ := timeDistanceSq p q - spaceDistanceSq p q
+
+-- "m sees the Minkowski metric between events as r"
+def minkowskiMetricSqEvents (m : B) (eventBody1 eventBody2 : B) (r : ℝ) : Prop :=
+  ∃ (x1 x2 : R4), W m eventBody1 x1 ∧ W m eventBody2 x2 ∧ minkowskiMetricSqPoints x1 x2 = r * r
+
 
 -- AXIOM 1: "For any inertial observer, the speed of light is 1. Furthermore, it is possible to
 --           send out a light signal in any direction."
@@ -60,84 +85,3 @@ abbrev axsm := ∀ (m k : B), IOb B IB W m ∧ IOb B IB W k →
 -- END AXIOM
 
 abbrev SpecRel := axph B IB Ph W ∧ axev B IB W ∧ axsf B IB W ∧ axsm B IB W
-
-
-
-
-/-
--- Theorem: "In special relativity, no inertial observer can travel faster than the speed of light
---           relative to another inertial observer."
-theorem slowerThanLight : SpecRel B IB Ph W → ∀ (m k : B), ∀ (x y : R4),
-  W m k x ∧
-  W m k y ∧
-  x ≠ y ∧
-  IOb B IB W m ∧
-  IOb B IB W k → spaceDistanceSq x y < timeDistanceSq x y := by sorry
-
-----------------------------------------------------------
-
--- "m sees k moving at velocity v"
-def velocitySq (m k : B) (v : ℝ) : Prop := ∀ (x y : R4), W m k x ∧ W m k y →
-  spaceDistanceSq x y = v * v * timeDistanceSq x y
-
--- "m sees eventBody at time t"
-def timeOfEvent (m eventBody : B) (t : ℝ) : Prop := ∃ (x : R4), W m eventBody x ∧ x 3 = t
-
--- Theorem: "In special relativity, different inertial observers may disagree on the temporal order of events"
-theorem temporalOrderParadox : SpecRel B IB Ph W → ∀ (m k : B) (v : ℝ),
-  IOb B IB W m ∧ IOb B IB W k ∧ velocitySq B W m k v ∧ v > 0 →
-  ∃ (b1 b2: B) (t1 t2 t3 t4: ℝ), timeOfEvent B W m b1 t1 ∧ timeOfEvent B W m b2 t2 ∧
-  timeOfEvent B W k b1 t3 ∧ timeOfEvent B W k b2 t4 ∧ t1 < t2 ∧ t4 < t3 := by
-    intros h_specRel m k v h_conditions
-    have h_contradiction : spaceDistanceSq (0 : R4)
-      (EuclideanSpace.single 0 1) < timeDistanceSq (0 : R4) (EuclideanSpace.single 0 1) := by
-      have := h_specRel.2.2.2; specialize this m m;
-      simp_all +decide [ Set.ext_iff ] ;
-      contrapose! this;
-      use 0, 0, 0, EuclideanSpace.single 0 1; simp_all +decide [ events ] ;
-      unfold spaceDistanceSq; norm_num [ spatial ] ;
-      unfold spaceNormSq; norm_num [ spatial ] ;
-      norm_num [ Fin.ext_iff ]
-    exfalso;
-    unfold spaceDistanceSq at h_contradiction; norm_num [ spatial ] at h_contradiction ;
-    unfold timeDistanceSq at h_contradiction; norm_num [ spatial ] at h_contradiction ;
-    unfold spaceNormSq at h_contradiction; norm_num [ spatial ] at h_contradiction ;
-    simp at h_contradiction;
-    linarith
-
-----------------------------------------------------
-
--- "m sees eventBody at location x"
-def locOfEvent (m : B) (eventBody : B) (x : R4) : Prop := W m eventBody x
-
--- "m sees the time between events as t"
-def timeBetweenEvents (m eventBody1 eventBody2 : B) (t : ℝ) : Prop :=
-  ∃ (x1 x2 : R4), W m eventBody1 x1 ∧ W m eventBody2 x2 ∧ timeDistanceSq x1 x2 = t * t
-
--- "m sees the spatial distance between events as s"
-def spaceBetweenEvents (m eventBody1 eventBody2 : B) (s : ℝ) : Prop :=
-  ∃ (x1 x2 : R4), W m eventBody1 x1 ∧ W m eventBody2 x2 ∧ spaceDistanceSq x1 x2 = s * s
-
--- the Minkowski metric between two points in R4
-def minkowskiMetricSqPoints (p q : R4) : ℝ := timeDistanceSq p q - spaceDistanceSq p q
-
--- "m sees the Minkowski metric between events as r"
-def minkowskiMetricSqEvents (m : B) (eventBody1 eventBody2 : B) (r : ℝ) : Prop :=
-  ∃ (x1 x2 : R4), W m eventBody1 x1 ∧ W m eventBody2 x2 ∧ minkowskiMetricSqPoints x1 x2 = r * r
-
--- "all inertial observers see the same Minkowski metric between events"
-theorem minkowskiMetricInvariant B IB Ph W : SpecRel B IB Ph W →
-  ∀ (m k : B) (eventBody1 eventBody2 : B) (r : ℝ),
-  IOb B IB W m ∧ IOb B IB W k → ((minkowskiMetricSqEvents B W m eventBody1 eventBody2 r) ↔
-                                (minkowskiMetricSqEvents B W k eventBody1 eventBody2 r)) := by
-    contrapose!;
-    simp +zetaDelta at *;
-    intro m k hm hk eventBody1 eventBody2 r h;
-    intro h1 h2 h3;
-    use m, hm, m, hm;
-    use 0; norm_num [ events ];
-    refine' ⟨ EuclideanSpace.single 0 1, _, 0, 0, _, _, _ ⟩ <;> norm_num [ spaceDistanceSq ];
-    · decide +revert;
-    · unfold spaceNormSq; norm_num [ spatial ] ;
-      norm_num [ Fin.ext_iff ]
--/
--- tempRelativity.lean
+++ tempRelativity.lean
@@ -1,4 +1,5 @@
-import Relativity.relativity
+import Relativity.lemmas
+import Relativity.definitions
 noncomputable section
 set_option relaxedAutoImplicit true
 
@@ -6,3 +7,26 @@ variable (B : Type) -- Bodies
 variable (IB : B → Prop) -- Inertial bodies predicate
 variable (Ph : B → Prop) -- Photon predicate
 variable (W : B → B → R4 → Prop) -- Worldview predicate
+
+-- Theorem: "In special relativity, no inertial observer can travel faster than the speed of light
+--           relative to another inertial observer."
+theorem slowerThanLight : SpecRel B IB Ph W → ∀ (m k : B), ∀ (x y : R4),
+  W m k x ∧
+  W m k y ∧
+  x ≠ y ∧
+  IOb B IB W m ∧
+  IOb B IB W k → spaceDistanceSq x y < timeDistanceSq x y := by sorry
+
+
+
+-- Theorem: "In special relativity, different inertial observers may disagree on the temporal order of events"
+theorem temporalOrderParadox : SpecRel B IB Ph W → ∀ (m k : B) (v : ℝ),
+  IOb B IB W m ∧ IOb B IB W k ∧ velocitySq B W m k v ∧ v > 0 →
+  ∃ (b1 b2: B) (t1 t2 t3 t4: ℝ), timeOfEvent B W m b1 t1 ∧ timeOfEvent B W m b2 t2 ∧
+  timeOfEvent B W k b1 t3 ∧ timeOfEvent B W k b2 t4 ∧ t1 < t2 ∧ t4 < t3 := by sorry
+
+  -- "all inertial observers see the same Minkowski metric between events"
+theorem minkowskiMetricInvariant B IB Ph W : SpecRel B IB Ph W →
+  ∀ (m k : B) (eventBody1 eventBody2 : B) (r : ℝ),
+  IOb B IB W m ∧ IOb B IB W k → ((minkowskiMetricSqEvents B W m eventBody1 eventBody2 r) ↔
+                                (minkowskiMetricSqEvents B W k eventBody1 eventBody2 r)) := by sorry
